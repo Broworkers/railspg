ef842a78b6130800575cd83407e9d2e7fe13383c
Daniel Libanori <daniel@current.com.br>
2012-10-13 13:51:14 -0300

    We are not going to use Active::Record

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Railspg::Application.config.secret_token = '469da6cb5955eb3d140d899f76d23f2d5e1a91a6f70155e3fb3d996c633d5c43d58544bac172b6c9b919592b82013afa7a46a2eef381e29184a755f90d96fd12'
+Railspg::Application.config.secret_token = '528d9e25027a3802cfae71e73f316a8a429e9f66648f476b1d1e78a272abcda961d7bab928d6e0b114dd559f9e2477c74f29e1ea2c69d7b38d684b72740e7626'
