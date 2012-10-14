28750cd2562e9a489adbc6dfd82b51552312f2cc
Daniel Libanori <daniel@current.com.br>
2012-10-14 19:49:43 -0300

    Allow auth on dev

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
   provider :twitter, 'OtgUJ38USbc3PnbRH1i9DA', 'sZGgzbqX3xPhzoVru6uzpIvrBwIRIQbrHVDG5CXrm8'
 end
