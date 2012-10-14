b3109e019949a464fdd6bd8df16742df44daf0a1
Daniel Libanori <daniel@current.com.br>
2012-10-14 15:12:29 -0300

    Making dm, host and me dynamically

--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -1,2 +1,9 @@
 module ChatsHelper
+  def classes_for(another)
+    [].tap do |classes|
+      classes << 'dm' if another.dm?
+      classes << 'host' if another.host?
+      classes << 'me' if current_user == another
+    end
+  end
 end
