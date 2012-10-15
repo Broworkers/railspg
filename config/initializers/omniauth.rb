fb2c4c9f3c9a79033e9f8bdeeb1cbe0af6c2ec73
Rafael Masoni <rafaelmasoni@gmail.com>
2012-10-15 01:17:09 -0300

    Adding perhaps a better way to check the environment for oAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'OtgUJ38USbc3PnbRH1i9DA', 'sZGgzbqX3xPhzoVru6uzpIvrBwIRIQbrHVDG5CXrm8'
 end
 
-if ENV['RAILS_ENV'] == "production"
+if Rails.env.production?
   OmniAuth.config.full_host = "http://railspg.com"
 else
   OmniAuth.config.full_host = "http://localhost:3000"
