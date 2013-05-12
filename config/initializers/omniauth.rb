bc5ebfbc29632113d667515f6f23140b7e9b7be8
Daniel Libanori <daniellibanori@gmail.com>
2013-05-12 19:08:34 -0300

    Using only pow at omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,10 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 end
 
 if Rails.env.production?
-  OmniAuth.config.full_host = "http://railspg.com"
-  -# OmniAuth.config.full_host = "http://railspg-rpg.herokuapp.com"
+  OmniAuth.config.full_host = 'http://railspg.com'
 else
-  OmniAuth.config.full_host = "http://localhost:3000"
-  -# For Pow users
-  -# OmniAuth.config.full_host = "http://railspg.dev"
+  OmniAuth.config.full_host = 'http://railspg.dev'
 end
